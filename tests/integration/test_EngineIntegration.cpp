c229532a4ce9e89e0f2d24e99b6c7177d5bb3675
Darleanow <enzohugonn@gmail.com>
2026-06-12 21:04:17 +0200

    fix(CI): Fix

--- tests/integration/test_EngineIntegration.cpp
+++ tests/integration/test_EngineIntegration.cpp
@@ -3,19 +3,21 @@
 
 #include "Alcia/core/Engine.hpp"
 
-#include <QCoreApplication>
+#include <QApplication>
 
 #include <gtest/gtest.h>
 
 class EngineTest : public ::testing::Test {
 protected:
     void SetUp() override {
-        int argc    = 0;
-        char** argv = nullptr;
-        app_        = std::make_unique<QCoreApplication>(argc, argv);
+        static int argc    = 0;
+        static char** argv = nullptr;
+        if (QApplication::instance() == nullptr) {
+            app_ = std::make_unique<QApplication>(argc, argv);
+        }
     }
 
-    std::unique_ptr<QCoreApplication> app_;
+    std::unique_ptr<QApplication> app_;
 };
 
 TEST_F(EngineTest, EngineConstructsAndRuns) {
